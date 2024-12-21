47825e27655c05530bcdab73c21411ee3a1b7731
Rogerio Romao <58592935+rogerio-romao@users.noreply.github.com>
2024-12-21 14:41:56 +0000

    Install and setup prettier for code formatting

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
-import { vitePlugin as remix } from '@remix-run/dev';
-import { defineConfig } from 'vite';
-import tsConfigPaths from 'vite-tsconfig-paths';
+import { vitePlugin as remix } from "@remix-run/dev";
+import { defineConfig } from "vite";
+import tsConfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-    plugins: [remix(), tsConfigPaths()],
+  plugins: [remix(), tsConfigPaths()],
 });
